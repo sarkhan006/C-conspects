0f441441efb94d2b3be61370b7e834eef19c716d
Sarkhan <sarkhan.eyn@gmail.com>
2022-08-25 00:13:40 +0200

    First Lesson

---
+++ FirstLesson/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+// 1'st task
+
+
+Console.Write("Input number - ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat = number * number;
+
+Console.WriteLine($"the quad of {number} is {kvadrat}");
+
+//Console.WriteLine("the quad of " + number + " is " + kvadrat);
+*/
+
+/*
+// 2'nd task
+Console.Write("Provide 1'st number - ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Provide second number - ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat = number2 * number2 ;
+
+if(number1 == kvadrat)
+{
+    Console.Write("Fist number is the quad of second");
+}
+else
+{
+    Console.Write("First number is NOT the quad of second");
+}
+*/
+
+
+//3'rd task
+
+Console.Write("Provide number - ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int current = number1 * (-1); 
+
+while(current <= number1)
+{
+    Console.Write(current + "  ");
+    current = current + 1;
+}
+
+
