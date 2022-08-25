87f30c82d1c524d9b9b9a9163f16220b52648841
sarkhan006 <110635128+sarkhan006@users.noreply.github.com>
2022-08-25 02:44:41 +0200

    corrected the mistakes

--- FirstLesson/FirstLessonHomework/Program.cs
+++ FirstLesson/FirstLessonHomework/Program.cs
@@ -77,21 +77,21 @@ else
 /* Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
-
+/*
 Console.Write("Provide number - ");
 int number = Convert.ToInt32(Console.ReadLine());
 
 int current = 1;
 
-while( current < number )
+while( current <= number )
 {
+    current =current + 1;
     if (current %2 == 0) 
 {
-    Console.Write( current + ' ');
-    current = current + 1;
+    Console.Write( current );
 }
 }
-
+*/
 
 
 
